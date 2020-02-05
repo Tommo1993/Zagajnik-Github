f0384faf17fc4e97f9a817e99eb9c9b57199f9bb
Tommo1993 <10x10tt@gmail.com>
2020-02-05 17:24:33 +0100

    05.02.2020 Wiosenne porządki.

--- Zagajnik (Overhaul)/data/enemydefs.py
+++
@@ -1,9 +0,0 @@
-class EnemyStats:
-	def __init__(entity, name, ID, HP, MP, attackdmg, EXPreward):
-		entity.name = name
-		entity.ID = ID
-		entity.HP = HP
-		entity.MP = MP
-		entity.attackdmg
-		entity.EXPreward
-        
