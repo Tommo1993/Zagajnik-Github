991e74c717de48771e2f60127511544fda1f61a2
Tommo1993 <10x10tt@gmail.com>
2020-02-08 22:41:27 +0100

    08.02.2020 System statystyk dodany.

--- Zagajnik (Overhaul)/ZAGAJNIK.py
+++ Zagajnik (Overhaul)/ZAGAJNIK.py
@@ -6,6 +6,173 @@ from time import sleep
 from data.itemdefs import items
 from data.inventory import inventory
 
+#####STATYSTYKI GRACZA#####
+class PlayerStats:
+    def __init__(self, name, HP, MP, MaxHP, MaxMP, XP, level, str, dex, int, end, luck, basedamage, statpoints, spentstatpoints, pclass, subclass, combatstate, pointsspentstr, pointsspentdex, pointsspentint, pointsspentend, pointsspentluck, armorhead, armortorso, armorarms, armorlegs, eqaddedstr, eqaddeddex, eqaddedint, eqaddedend, eqaddedluck, basestr, basedex, baseint, baseend, baseluck):
+        self.name = name
+        self.HP = HP
+        self.MP = MP
+        self.MaxHP = MaxHP
+        self.MaxMP = MaxMP
+        self.XP = XP
+        self.level = level
+        self.str = str
+        self.dex = dex
+        self.int = int
+        self.end = end
+        self.luck = luck
+        self.basedamage = basedamage
+        self.statpoints = statpoints
+        self.spentstatpoints = spentstatpoints
+        self.pclass = pclass
+        self.subclass = subclass
+        self.combatstate = combatstate
+        self.pointsspentstr = pointsspentstr
+        self.pointsspentdex = pointsspentdex
+        self.pointsspentint = pointsspentint
+        self.pointsspentend = pointsspentend
+        self.pointsspentluck = pointsspentluck
+        self.armorhead = armorhead #zależne od ekwipunku
+        self.armortorso = armortorso #
+        self.armorarms = armorarms #
+        self.armorlegs = armorlegs #
+        self.eqaddedstr = eqaddedstr # 
+        self.eqaddeddex = eqaddeddex #
+        self.eqaddedint = eqaddedint #
+        self.eqaddedend = eqaddedend #
+        self.eqaddedluck = eqaddedluck #
+        self.basestr = basestr
+        self.basedex = basedex
+        self.baseint = baseint
+        self.baseend = baseend
+        self.baseluck = baseluck
+    
+    def classchoice(self): #wybór startowej klasy, przedmioty mogą być dodane za pomocą tego (prawdopodobnie)
+        print("Wybierz swoją początkową klasę.")
+        print("Masz do wyboru zbrojnego, zwiadowcę i adepta magii.")
+        print("(1/2/3)")
+        while True:
+            qwe = str(input())
+            if qwe == "1":
+                self.pclass = 1
+                self.subclass = 1
+                self.HP = 20 #maxhp się zwiększa wraz z siłą i wytrzymałością, podobnie jak mana; 4 (str) * 1 + 3 (end) * 2 = dodane maxhp
+                self.MaxHP = 10 #
+                self.MP = 5
+                self.MaxMP = 0
+                self.basestr = 4
+                self.baseend = 3
+                self.baseint = 1
+                self.basedex = 3
+                self.baseluck = 1
+                print("Wybrałeś zbrojnego.")
+                break
+            if qwe == "2":
+                self.pclass = 2
+                self.subclass = 1
+                self.HP = 15
+                self.MaxHP = 10
+                self.MP = 10
+                self.MaxMP = 0
+                self.basestr = 3
+                self.baseend = 1
+                self.baseint = 3
+                self.basedex = 4
+                self.baseluck = 2
+                print("Wybrałeś zwiadowcę.")
+                break
+            if qwe == "3":
+                self.pclass = 3
+                self.subclass = 1
+                self.HP = 10
+                self.MaxHP = 6
+                self.MP = 15
+                self.MaxMP = 0
+                self.basestr = 2
+                self.baseend = 1
+                self.baseint = 6
+                self.basedex = 3
+                self.baseluck = 1
+                print("Wybrałeś adepta magii.")
+                break
+        return self
+    def calcstats(self): #oblicza statystyki, by wszystko się zgadzało
+        self.str = self.pointsspentstr + self.eqaddedstr + self.basestr
+        self.dex = self.pointsspentdex + self.eqaddeddex + self.basedex
+        self.int = self.pointsspentint + self.eqaddedint + self.baseint
+        self.end = self.pointsspentend + self.eqaddedend + self.baseend
+        self.luck = self.pointsspentluck + self.eqaddedluck + self.baseluck
+        return self
+        
+    def calcmaxhp(self):
+        self.MaxHP = 10+(1*self.str)+(2*self.end)
+        if self.HP > self.MaxHP:
+            self.HP = self.MaxHP
+        if self.MaxHP < 0:
+            print("Ta wiadomość nie powinna się pokazywać. Coś jest nie tak z twoim maksymalnym poziomem życia...")
+            self.MaxHP = 1
+            print("Ustawiono maksymalne życie na: 1")
+        return self
+    def calcmaxmp(self):
+        self.MaxMP = 0+(1*self.dex)+(2*self.int)
+        if self.MP > self.MaxMP:
+            self.MP = self.MaxMP
+        if self.MaxMP < 0:
+            print("Ta wiadomość nie powinna się pokazywać. Coś jest nie tak z twoim maksymalnym poziomem many...")
+            self.MaxMP = 0
+            print("Ustawiono maksymalną manę na: 0")
+        return self
+    def calcdamage(self):
+        self.basedamage = 1+(1*self.dex)+(2*self.str)
+        if self.basedamage < 0:
+            print("Ta wiadomość nie powinna się pokazywać. Coś jest nie tak z twoimi obrażeniami podstawowymi...")
+            self.MaxMP = 1
+            print("Ustawiono podstawowe obrażenia na: 1")
+        return self
+    def calclevel(self):
+        if self.XP < 0:
+            print("Ta wiadomość nie powinna się pokazywać. Coś jest nie tak z twoim poziomem...")
+        if self.XP == 0:
+            self.level = 0
+            self.statpoints = 0
+        if self.XP > 0 and self.XP <= 99:
+            self.level = 1
+            self.statpoints = 1 - self.spentstatpoints
+        if self.XP >= 100 and self.XP <= 219:
+            self.level = 2
+            self.statpoints = 2 - self.spentstatpoints
+        if self.XP >= 220 and self.XP <= 349:
+            self.level = 3
+            self.statpoints = 3 - self.spentstatpoints
+        if self.XP >= 350 and self.XP <= 499:
+            self.level = 4
+            self.statpoints = 4 - self.spentstatpoints
+        if self.XP >= 500 and self.XP <= 750:
+            self.level = 5
+            self.statpoints = 5 - self.spentstatpoints
+        return self
+    def displaystats(self):
+        print(player.name, "nazwa gracza")
+        print(player.HP, "hp")
+        print(player.MP, "mp")
+        print(player.MaxHP, "maxhp")
+        print(player.MaxMP, "maxmp")
+        print(player.XP, "xp")
+        print(player.level, "lvl")
+        print(player.str, "str")
+        print(player.dex, "dex")
+        print(player.int, "int")
+        print(player.end, "end")
+        print(player.luck, "luck")
+        print(player.basedamage, "basedmg")
+        print(player.statpoints, "statp")
+        print(player.spentstatpoints, "spentstatp")
+        print(player.pclass, "pclass")
+        print(player.subclass, "subclass")
+        print(player.combatstate, "combatstate")
+
+
+#####STATYSTYKI GRACZA#####
 #####SYSTEM QUESTÓW#####
 def Questy():
         
@@ -145,60 +312,6 @@ def SzukanieMiejscaNaPrzedmiot(KoniecQuesta):
         #   SzukanieMiejscaNaPrzedmiot(item, KoniecQuesta)
 #####SYSTEM QUESTÓW#####
 
-#####GRACZ#####
-class PlayerStats:
-    def __init__(player, name, HP, MP, MaxHP, MaxMP, MaxHPnocalc, MaxMPnocalc, EXP, level, str, dex, int, end, luck, basedamage, weapondamage, statpoint, pclass, subclass):
-        player.name = name
-        player.HP = HP
-        player.MP = MP
-        player.MaxHP = MaxHP
-        player.MaxMP = MaxMP
-        player.MaxHPnocalc = MaxHPnocalc
-        player.MaxMPnocalc = MaxMPnocalc
-        player.EXP = EXP
-        player.level = level
-        player.str = str
-        player.dex = dex
-        player.int = int
-        player.end = end
-        player.luck = luck
-        player.basedamage = basedamage
-        player.weapondamage = weapondamage
-        player.statpoint = statpoint
-        player.pclass = pclass
-        player.subclass = subclass
-def ClassChoice(player,StartInput):		
-    if StartInput == "1" or StartInput == "zbrojny":
-        print("Twoja klasa to zbrojny.")
-        player.MaxHP == 20
-        player.HP == 20
-        player.MaxMP == 5
-        player.MP == 5
-        player.str = 3
-        player.dex = 2
-        player.int = 1
-        player.end = 2
-        player.luck = 1
-        player.pclass = 1
-    if StartInput == "2" or StartInput == "zwiadowca":
-        rint("Twoja klasa to zwiadowca.")
-        player.MaxHP == 15
-        player.HP == 15
-        player.MaxMP == 10
-        player.MP == 5
-        player.pclass = 2
-    if StartInput == "3" or StartInput == "łotr":
-        print("Twoja klasa to łotr.")
-        player.MaxHP == 10
-        player.HP == 10
-        player.MaxMP == 15
-        player.MP == 15
-        player.pclass = 3
-    return player
-
-#####GRACZ#####
-
-
 #####GŁÓWNY MODUŁ#####
 akcja=['Zjedz','Użyj','Opis','Atakuj','Zagadaj']
 OtherAction=['zjedz','użyj','opis','atakuj','zagadaj', 'ubierz', 'dobądź', 'zdejmij', 'schowaj', 'ekwipunek', 'weź']
@@ -267,9 +380,49 @@ YS = ['S','s']
 XA = ['A','a']
 XD = ['D','d']
 
-
+player = PlayerStats("",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+player.name = str(input("Powiedz jak się nazywasz podróżniku.")) #możecie to dać pod komentarzem jak przeszkadza
+player.classchoice() #to też możecie dać pod komentarzem
 while True:
-    ActionInput = str(input())
+    player.calcstats()
+    player.calcmaxhp()
+    player.calcmaxmp()
+    player.calcdamage()
+    player.calclevel()
+    ActionInput = input("[{} HP] [{} MP] [{} XP] [{} LVL] {}".format(player.HP,player.MP,player.XP,player.level,"\n")).lower()
+    #zmieniłem inputa, powinien działać tak samo
+    if x == "stats":
+        player.displaystats()
+    if x == "addxp":
+        player.XP += 50
+        print("dodano 50 xp")
+    if x == "lvlup" and player.statpoints <= 0:
+        print("nie masz wystarczająco punktów")
+    if x == "lvlup" and player.statpoints > 0:
+        y = input("wybierz, którą statystykę chcesz ulepszyć (str/dex/int/end/luck)")
+        if y == "str":
+            player.pointsspentstr += 1
+            player.spentstatpoints += 1
+            player.calclevel()
+        if y == "dex":
+            player.pointsspentdex += 1
+            player.spentstatpoints += 1
+            player.calclevel()
+        if y == "int":
+            player.pointsspentint += 1
+            player.spentstatpoints += 1
+            player.calclevel()
+        if y == "end":
+            player.pointsspentend += 1
+            player.spentstatpoints += 1
+            player.calclevel()
+        if y == "luck":
+            player.pointsspentluck += 1
+            player.spentstatpoints += 1
+            player.calclevel()
+        if y not in ("str","dex","int","end","luck"):
+            print("zła odpowiedź")                         #kod co do statystyk gracza tu się kończy
+            
     items()
     itemlist = items()
     inventory(ekwipunek, ActionInput, itemlist)
